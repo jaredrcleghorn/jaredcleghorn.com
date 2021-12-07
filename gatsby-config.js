e7d449e92874f6501f9f543a4b571159e9b024a4
Jared Cleghorn <jaredrcleghorn@gmail.com>
2021-12-07 14:53:15 -0500

    add gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+  ],
 }

290013bef466ba105976bbc051c92fc5892dc008
Jared Cleghorn <jaredrcleghorn@gmail.com>
2021-12-07 14:43:02 -0500

    add "About Me" Card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,14 @@ import { Card, Layout } from "../components"
 export default function Home() {
   return (
     <Layout>
-      <Card title="Hello world!" />
+      <Card title="About Me">
+        <div className="mt-2">
+          Hi! My name is Jared Cleghorn&mdash;welcome to my website! I studied
+          physics, mathematics, and computer science at the University of
+          Alabama, and I worked as a student web developer for the University
+          Libraries.
+        </div>
+      </Card>
     </Layout>
   )
 }

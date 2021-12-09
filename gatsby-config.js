f47f5a4ffdae274e55f3f8e3ffb698f6fd78c0bc
Jared Cleghorn <jaredrcleghorn@gmail.com>
2021-12-09 11:04:28 -0500

    add gatsby-remark-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,11 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         excerpt_separator: `<!-- end -->`,
-        plugins: [`gatsby-remark-katex`, `gatsby-remark-prismjs`],
+        plugins: [
+          `gatsby-remark-images`,
+          `gatsby-remark-katex`,
+          `gatsby-remark-prismjs`,
+        ],
       },
     },
   ],
